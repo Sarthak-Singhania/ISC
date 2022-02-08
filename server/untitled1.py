a6177c88ff4437214c1404a1305e7069ce8da35c
sarthak2806 <sathak2806singhania@gmail.com>
2022-02-08 09:54:27 +0530

    delete

--- server/untitled1.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request,make_response
-app=Flask(__name__)
-
-@app.route('/hello')
-def hello():
-    return make_response({'message':request.args.get('list').split(',')})
-
-if __name__=='__main__':
-    app.run(debug=True)
