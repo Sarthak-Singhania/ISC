4f967f2c1b723f88411fed4767981ecfca630daf
Sarthak-Singhania-Bungee <sarthak@bungeetech.com>
2022-10-29 11:21:26 +0530

    update

--- server/try.py
+++ server/try.py
@@ -1,6 +1,6 @@
 import mysql.connector as sql
 db=sql.connect(host='68.183.244.199',user='u724843278_ISC',password='ISCdatabase@1234',database='u724843278_ISC')
 cur=db.cursor(dictionary=True)
-cur.execute('show tables')
-for i in cur:
-    print(i)
+cur.execute('select * from bookings')
+bookings=cur.fetchall()
+print(bookings)
